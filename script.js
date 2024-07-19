e131f1aad3e346568885f17a69b76ee0d16336f3
Mark Quirante <quir0054@algonquinlive.com>
2024-07-19 19:17:22 -0400

    Changed API source to Food Meals and added Search functionality by category

--- script.js
+++ script.js
@@ -1,44 +1,75 @@
-document
-	.getElementById("searchForm")
-	.addEventListener("submit", function (event) {
-		event.preventDefault(); // Prevent the form from submitting in the traditional way
+/** Food Categories Functionality */
 
-		// Get the value of the search input field
-		let searchQuery = document.getElementById("searchInput").value;
-
-		// Pass the search query to a function
-		handleSearch(searchQuery);
-	});
-
-function handleSearch(breedName) {
-	console.log(breedName);
-
-	// Calls function and fetches the dog image
-	getDogImage(breedName);
-}
-
-function formatBreedURL(breedName) {
-	const ApiUrl = "https://dog.ceo/api/breed/" + breedName + "/images/random";
-	return ApiUrl;
-}
-
-async function getDogImage(breedName) {
-	const ApiUrl = formatBreedURL(breedName);
-
+const APICategories = "https://www.themealdb.com/api/json/v1/1/categories.php";
+async function getFoodCategories() {
 	try {
-		const response = await fetch(ApiUrl);
+		const response = await fetch(APICategories);
 
 		if (!response.ok) {
 			throw new Error(`HTTP error! Status: ${response.status}`);
 		}
-
 		const data = await response.json();
-		let dogImage = data.message;
-
-		const breedImageContainer = document.getElementById("dog-image");
-		breedImageContainer.src = dogImage;
-		breedImageContainer.alt = "Picture of Dog";
+		return data.categories;
 	} catch (error) {
 		console.error("Error fetching data:", error); // Handle errors
+	}
+}
+
+async function displayFoodCategoryName() {
+	const foodCategoriesList = document.getElementById("food-categories-list");
+	foodCategoriesList.innerHTML = ""; // Clear the list first
+	const categories = await getFoodCategories();
+	for (let i = 0; i < categories.length; i++) {
+		if (categories[i]) {
+			const listItem = document.createElement("li");
+			listItem.textContent = categories[i].strCategory;
+			foodCategoriesList.appendChild(listItem);
+		}
+	}
+}
+
+displayFoodCategoryName();
+
+/** Search Bar Functionality */
+
+const APISearchCategoryURL =
+	"https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+
+async function getSearch(category) {
+	try {
+		const response = await fetch(APISearchCategoryURL + category);
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+		const data = await response.json();
+		console.log(data);
+		return data.meals;
+	} catch (error) {
+		console.error("Error fetching data:", error); // Handle errors
+	}
+}
+
+const searchForm = document.getElementById("category-search-from");
+searchForm.addEventListener("submit", displaySearch);
+
+async function displaySearch(event) {
+	event.preventDefault();
+
+	const userInput = document.getElementById("category-search").value;
+	const userSearch = await getSearch(userInput);
+
+	const display = document.getElementById("display-search");
+
+	display.innerHTML = "";
+	for (let i = 0; i < 5; i++) {
+		if (userSearch[i]) {
+			const listItem = document.createElement("li");
+			const listImage = document.createElement("img");
+			listItem.textContent = userSearch[i].strMeal;
+			listImage.src = userSearch[i].strMealThumb;
+			display.appendChild(listItem);
+			display.appendChild(listImage);
+		}
 	}
 }
