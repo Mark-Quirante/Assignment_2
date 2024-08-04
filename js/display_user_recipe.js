f11727a488bf6315f28365063216ce4e3b698183
Mark Quirante <quir0054@algonquinlive.com>
2024-08-04 15:00:10 -0400

    Feat: Added overlay window in saved recipes page

--- js/display_user_recipe.js
+++ js/display_user_recipe.js
@@ -1,4 +1,7 @@
 const SAVED_RECIPE_DISPLAY = document.getElementById("user-saved-recipe-ul");
+const OVERLAY_WINDOW = document.getElementById("ingredient-overlay");
+const CLOSE_OVERLAY_BUTTON = document.getElementById("exit-overlay-button");
+const RECIPE_OVERLAY = document.getElementById("recipe-list-ingredient");
 
 async function displaySavedRecipes() {
 	const savedRecipes = await getSavedRecipes();
@@ -29,7 +32,64 @@ async function displaySavedRecipes() {
 		SAVED_RECIPE_DISPLAY.appendChild(listName);
 		listName.appendChild(listImg);
 		listName.appendChild(removeButton);
+
+		//adding data attributes to elements
+		listName.dataset.mealId = savedRecipes[i].mealID;
+		listImg.dataset.mealId = savedRecipes[i].mealID;
 	}
 }
 
-displaySavedRecipes();
+async function onClickMealList(event) {
+	const mealId = event.target.dataset.mealId;
+	RECIPE_OVERLAY.innerHTML = ""; // Clear list of Ingredients first
+	if (mealId) {
+		const ingredients = await getMealIngredients(mealId);
+		const nameMeal = ingredients.strMeal;
+		const nameElement = document.createElement("h2");
+		const areaMeal = ingredients.strArea;
+		const areaElement = document.createElement("h3");
+
+		nameElement.innerHTML = nameMeal;
+		areaElement.innerHTML = areaMeal;
+
+		RECIPE_OVERLAY.appendChild(nameElement);
+		RECIPE_OVERLAY.appendChild(areaElement);
+
+		displayIngredientsAndMeasurments(ingredients);
+		OVERLAY_WINDOW.style.display = "flex";
+	}
+}
+
+async function displayIngredientsAndMeasurments(ingredients) {
+	for (let i = 1; i <= 20; i++) {
+		const ingredient = ingredients["strIngredient" + i];
+		const measurement = ingredients["strMeasure" + i];
+
+		const listElement = document.createElement("li");
+		const ingredientElement = document.createElement("p");
+		const measureElement = document.createElement("p");
+
+		ingredientElement.innerHTML = ingredient;
+		measureElement.innerHTML = measurement;
+		listElement.appendChild(ingredientElement);
+		listElement.appendChild(measureElement);
+		RECIPE_OVERLAY.appendChild(listElement);
+	}
+	const mealInstructions = ingredients.strInstructions;
+	const mealInstructionsElement = document.createElement("p");
+	mealInstructionsElement.classList.add("mealInstructions");
+
+	mealInstructionsElement.innerHTML = mealInstructions;
+	RECIPE_OVERLAY.appendChild(mealInstructionsElement);
+}
+
+function hideOverlayWindow() {
+	OVERLAY_WINDOW.style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	CLOSE_OVERLAY_BUTTON.addEventListener("click", hideOverlayWindow);
+	SAVED_RECIPE_DISPLAY.addEventListener("click", onClickMealList);
+
+	displaySavedRecipes();
+});
